3c7ec497e0835bbe1e9a185630ce84d085b3f24f
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-12-27 22:14:07 +0530

    create projrct

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import './Screens/Homepage.dart';
+
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
