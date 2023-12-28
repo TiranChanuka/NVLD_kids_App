1db9ad4ab8b716720d6c6ee48863bd5bf04736db
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-12-29 00:28:15 +0530

    Create NaviationBar.dart

---
+++ lib/NaviationBar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:nvld_app/color.dart';
+import './Screens/Homepage.dart';
+import './Screens/GamePage.dart';
+import './Screens/ProfilePage.dart';
+import './Screens/StatPage.dart';
+
+class NavigationBarBottom extends StatefulWidget {
+  const NavigationBarBottom({super.key});
+  @override
+  State<NavigationBarBottom> createState() => _NavigationBarBottomState();
+}
+
+class _NavigationBarBottomState extends State<NavigationBarBottom> {
+  int currentIndex = 0;
+  List Pages = const [
+    HomeScreen(),
+    Games(),
+    Profile(),
+    StatShow(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          selectedItemColor: tertiaryColor,
+          unselectedItemColor: primaryColor,
+          selectedFontSize: 15,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_esports_outlined),
+              label: 'Games',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.analytics_outlined),
+              label: 'Stats',
+            ),
+          ],
+        ),
+      ),
+      body: Pages[currentIndex],
+    );
+  }
+}
