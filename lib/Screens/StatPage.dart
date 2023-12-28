e36a3e5a49d05a75ecef7fb51b2f0ef1def69eea
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-12-29 00:28:25 +0530

    Create StatPage.dart

---
+++ lib/Screens/StatPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatShow extends StatelessWidget {
+  const StatShow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
