8c1571b1d1726a87097c8ca034e3d22c0d901ced
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-12-30 00:09:37 +0530

    Update GamePage.dart

--- lib/Screens/GamePage.dart
+++ lib/Screens/GamePage.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Games extends StatelessWidget {
   const Games({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.red,
+    return Scaffold(
+      body: ListView(
+        children: [
+          // Load a Lottie file from your assets
+          Lottie.asset('assets/animations/kidsworking.json'),
+
+        ],
+      ),
     );
   }
 }
