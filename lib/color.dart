59c997e0518c6f41a2fc04892049d19bcf08622d
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-12-30 23:23:03 +0530

    Update color.dart

--- lib/color.dart
+++ lib/color.dart
@@ -5,3 +5,16 @@ const Color secondaryColor = Color(0xFF8CE3A1);
 const Color tertiaryColor = Color(0xFFF79F28);
 const Color quaternaryColor = Color(0xFF4CAF50);
 const Color whitecolor = Color(0xFFF2F2F2);
+
+// Gradients
+const LinearGradient primaryGradient = LinearGradient(
+  colors: [primaryColor, secondaryColor],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
+
+const LinearGradient secondaryGradient = LinearGradient(
+  colors: [secondaryColor, primaryColor],  // Reversed order for variation
+  begin: Alignment.topRight,
+  end: Alignment.bottomLeft,
+);
