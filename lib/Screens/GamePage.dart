d2422598413a8d2dcfa2bbcdd52f99fd84254878
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-01-11 00:05:48 +0530

    Update GamePage.dart

--- lib/Screens/GamePage.dart
+++ lib/Screens/GamePage.dart
@@ -8,41 +8,51 @@ class Games extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whitecolor,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: whitecolor,
+        title: Text(
+          'Games',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: primaryColor,
+          ),
+        ),
+      ),
       body: ListView(
         children: [
           Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Games',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: primaryColor,
-              ),
-            ),
+            padding: const EdgeInsets.all(0),
+            child: Lottie.asset('assets/animations/ani5.json'),
           ),
-          Lottie.asset('assets/animations/play.json'),
+          SizedBox(height: 10),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.only(left: 20, right: 20),
             child: Text(
               'Games is a game that helps children with NVLD to improve their social skills.',
               textAlign: TextAlign.start,
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.w400,
-                color: quaternaryColor,
+                color: primaryColor,
               ),
             ),
           ),
+          SizedBox(height: 10),
           Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text('Select Catagory',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 24,
-                    color: tertiaryColor,
-                    fontWeight: FontWeight.bold)),
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Text(
+              'Select Catagory',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: tertiaryColor,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
+          SizedBox(height: 10),
         ],
       ),
     );
