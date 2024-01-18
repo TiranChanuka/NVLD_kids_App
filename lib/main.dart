3a57991e39d214eadbbda31451e0e59308d5a4ba
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-01-18 19:47:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:nvld_app/BubblePopBliss/BubblePopBliss.dart';
+import 'package:nvld_app/DoodleDash/DoodleDashHome.dart';
 import 'package:nvld_app/FeelingExplore/FeelingExplorerHome.dart';
+import 'package:nvld_app/MyDayMyWay/MyDayMyWay.dart';
 import 'package:nvld_app/NaviationBar.dart';
 import './Screens/Homepage.dart';
 
 import 'package:device_preview/device_preview.dart';
 
+import 'MagicPattern/MagicPatternHome.dart';
+import 'PuzzlePlanet/PuzzelPlanetHome.dart';
 import 'SplashScreen/splashScreenOne.dart';
 
 void main() => runApp(
@@ -28,7 +33,11 @@ class MyApp extends StatelessWidget {
       home: SplashScreenOne(),
       routes: {
         '/feeling_explore': (context) => FeelingExplorer(),
-        // ... other routes
+        '/magic_pattern':(context) => MagicPatternHome(),
+        '/puzzel_planet':(context) => PuzzelPlanetHome(),
+        '/doodle_dash': (context) => DoodleDashHome(),
+        '/bubble_pop_bliss':(context) => BubblePopBliss(),
+        '/myday_myway':(context) => MyDayMyWay()
       },
     );
   }
