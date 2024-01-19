8ca664fbb99ad875e88f2099812972e03ced0725
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-01-19 23:20:29 +0530

    Update PuzzelPlanetHome.dart

--- lib/PuzzlePlanet/PuzzelPlanetHome.dart
+++ lib/PuzzlePlanet/PuzzelPlanetHome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nvld_app/Game/PuzzleGame.dart';
 
 import '../color.dart';
 
@@ -21,6 +23,30 @@ class PuzzelPlanetHome extends StatelessWidget {
             color: primaryColor,
           ),
         ),
+      ),
+      body: ListView(
+        children: [
+          ElevatedButton(
+            child: Text('Click Me'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PuzzleGame()),
+              );
+              print('Button pressed!');
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue, // Background color
+              onPrimary: Colors.white, // Text color
+              elevation: 5, // Shadow elevation
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15), // Corner rounding
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
