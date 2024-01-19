fe465a905c5b0ca2d539ee22f1894d27224be4a3
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-01-19 23:20:22 +0530

    Update color.dart

--- lib/color.dart
+++ lib/color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xFF004C8D);
+const Color primaryColor = Color(0xFF11249F);
 const Color secondaryColor = Color(0xFF8CE3A1);
 const Color tertiaryColor = Color(0xFFF79F28);
 const Color quaternaryColor = Color(0xFF4CAF50);
