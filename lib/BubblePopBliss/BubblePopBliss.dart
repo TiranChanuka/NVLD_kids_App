bb277e10e2fe561859093a9a8e1171fe3982b02e
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-01-25 22:20:32 +0530

    Update BubblePopBliss.dart

--- lib/BubblePopBliss/BubblePopBliss.dart
+++ lib/BubblePopBliss/BubblePopBliss.dart
@@ -22,6 +22,106 @@ class BubblePopBliss extends StatelessWidget {
           ),
         ),
       ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10,right: 10,),
+            child: Image.asset('assets/images/mindrelax.png'),
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Card(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1, // Adjust ratio as needed
+                    child: Image.asset('assets/images/smile.jpg',width: 50,height: 100, fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    flex: 2, // Adjust ratio as needed
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        'Card Title',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Card(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1, // Adjust ratio as needed
+                    child: Image.asset('assets/images/smile.jpg',width: 50,height: 100, fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    flex: 2, // Adjust ratio as needed
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        'Card Title',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Card(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1, // Adjust ratio as needed
+                    child: Image.asset('assets/images/smile.jpg',width: 50,height: 100, fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    flex: 2, // Adjust ratio as needed
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        'Card Title',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Padding(padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Card(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1, // Adjust ratio as needed
+                    child: Image.asset('assets/images/smile.jpg',width: 50,height: 100, fit: BoxFit.cover),
+                  ),
+                  Expanded(
+                    flex: 2, // Adjust ratio as needed
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        'Card Title',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
