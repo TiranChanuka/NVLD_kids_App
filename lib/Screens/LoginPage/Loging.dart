13197931a428f204cbdc05f0ea0813a0db1cb92b
Chalani Hansika <ict20068@std.uwu.ac.lk>
2024-02-01 11:58:44 +0530

    Update Loging.dart

--- lib/Screens/LoginPage/Loging.dart
+++ lib/Screens/LoginPage/Loging.dart
@@ -4,7 +4,7 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
           Text('Hello'),
