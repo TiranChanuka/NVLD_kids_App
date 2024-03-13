f3018c7425d50a53efa8cc495d39731f9779c49c
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-13 22:35:04 +0530

    Update ProfilePage.dart

--- lib/Screens/ProfilePage.dart
+++ lib/Screens/ProfilePage.dart
@@ -11,15 +11,19 @@ class Profile extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          SizedBox(height: 20,),
-          Padding(padding: EdgeInsets.all(20),
-          child: Text('Profile',
-            style: GoogleFonts.sourceSans3(
-              fontSize: 25,
-              fontWeight: FontWeight.w700,
-              color: textColor,
-            ),
+          SizedBox(
+            height: 20,
           ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Profile',
+              style: GoogleFonts.sourceSans3(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+                color: textColor,
+              ),
+            ),
           ),
           Padding(
             padding: EdgeInsets.all(10.0),
@@ -29,17 +33,29 @@ class Profile extends StatelessWidget {
                 color: cardColor,
                 borderRadius: BorderRadius.circular(10.0),
                 border: Border.all(
-                  color: Colors.grey,
+                  color: Colors.white,
                   width: 0,
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey
+                        .withOpacity(0.3), // Shadow color with opacity
+                    offset: Offset(
+                        4.0, 4.0), // Distance the shadow is offset from the box
+                    blurRadius: 5.0, // How blurry the shadow is
+                  ),
+                ],
               ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center, // Center content vertically
-                crossAxisAlignment: CrossAxisAlignment.center, // Center content horizontally
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center content vertically
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // Center content horizontally
                 children: [
                   // Display user image
                   ClipRRect(
-                    borderRadius: BorderRadius.circular(50.0), // Adjust as needed
+                    borderRadius:
+                        BorderRadius.circular(50.0), // Adjust as needed
                     child: Image.asset(
                       'assets/images/smile.png',
                       height: 100.0, // Adjust image height
@@ -47,36 +63,175 @@ class Profile extends StatelessWidget {
                       fit: BoxFit.cover, // Cover the entire area
                     ),
                   ),
-                  SizedBox(height: 20,),
-                  Text('Name',
-                  style: GoogleFonts.sourceSans3(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: textColor,
+                  SizedBox(
+                    height: 20,
                   ),
+                  IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                  Text(
+                    'Name',
+                    style: GoogleFonts.sourceSans3(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: textColor,
+                    ),
                   ),
                 ],
               ),
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              color: cardColor,
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Colors.grey,
-                width: 0,
-              ),
-            ),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(child: Text('Name',style: Theme.of(context).textTheme.bodySmall,overflow: TextOverflow.ellipsis,))
-                  ],
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: cardColor,
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 0,
                 ),
-              ],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey
+                        .withOpacity(0.3), // Shadow color with opacity
+                    offset: Offset(
+                        4.0, 4.0), // Distance the shadow is offset from the box
+                    blurRadius: 5.0, // How blurry the shadow is
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text('Personal Information',
+                      style: Theme.of(context).textTheme.headline6),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Name:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                          child: Text(
+                        'Jhone Doe',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                      Expanded(
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios_outlined),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
