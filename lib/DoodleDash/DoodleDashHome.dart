5d5228271c66ca626a031954faae93b03a5a7852
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-15 00:39:56 +0530

    Update DoodleDashHome.dart

--- lib/DoodleDash/DoodleDashHome.dart
+++ lib/DoodleDash/DoodleDashHome.dart
@@ -3,63 +3,89 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
 import '../color.dart';
- class DoodleDashHome extends StatelessWidget {
-   const DoodleDashHome({super.key});
+import 'drawing.dart';
 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         iconTheme: const IconThemeData(
-           color: primaryColor,
-         ),
-         // title: Text(
-         //   'Doodle Dash',
-         //   style: GoogleFonts.sourceSans3(
-         //     fontSize: 24,
-         //     fontWeight: FontWeight.w700,
-         //     color: primaryColor,
-         //   ),
-         // ),
-       ),
-       body: ListView(
-         children: [
-           Text('Fine Motor Skills Challenges',
-             textAlign: TextAlign.center,
-             style: GoogleFonts.sourceSans3(
-               fontSize: 25,
-               fontWeight: FontWeight.w700,
-               color: primaryColor,
-             ),
-           ),
-           Padding(
-             padding: const EdgeInsets.only(left: 20, right: 20),
-             child: Lottie.asset('assets/animations/ball.json',
-                 width: 300, height: 300),
-           ),
-           Padding(padding: const EdgeInsets.only(left: 20,right: 20),
-             child: Text('Activities that involve precise hand movements, like drawing, tracing, or drag-and-drop tasks, can help improve fine motor skills.',
-               style: GoogleFonts.sourceSans3(
-                 fontSize: 20,
-                 fontWeight: FontWeight.w500,
-                 color: primaryColor,
-               ),
-             ),
-           ),
-           const SizedBox(height: 20,),
-           const Padding(
-             padding: EdgeInsets.only(left: 10, right: 10),
-             child: Text(
-               'Select Catagory',
-               textAlign: TextAlign.center,
-               style: TextStyle(
-                   fontSize: 24,
-                   color: tertiaryColor,
-                   fontWeight: FontWeight.bold),
-             ),
-           ),
-         ],
-       ),
-     );
-   }
- }
+class DoodleDashHome extends StatelessWidget {
+  const DoodleDashHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: primaryColor,
+        ),
+        // title: Text(
+        //   'Doodle Dash',
+        //   style: GoogleFonts.sourceSans3(
+        //     fontSize: 24,
+        //     fontWeight: FontWeight.w700,
+        //     color: primaryColor,
+        //   ),
+        // ),
+      ),
+      body: ListView(
+        children: [
+          Text(
+            'Fine Motor Skills Challenges',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.sourceSans3(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+              color: primaryColor,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Lottie.asset('assets/animations/ball.json',
+                width: 300, height: 300),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Text(
+              'Activities that involve precise hand movements, like drawing, tracing, or drag-and-drop tasks, can help improve fine motor skills.',
+              style: GoogleFonts.sourceSans3(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: primaryColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: Text(
+              'Select Catagory',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: tertiaryColor,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DrawingBoard()),
+              );
+              print('Button pressed!');
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue, // Text color
+              elevation: 5, // Shadow elevation
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15), // Corner rounding
+              ),
+            ),
+            child: const Text('Drawing board'),
+          )
+        ],
+      ),
+    );
+  }
+}
