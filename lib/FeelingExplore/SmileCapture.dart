c020df46212baa87802d519b19bdb97b0c5d2fc4
Tiran Chanuka <tiranchanuka1998@gmail.com>
2024-03-15 18:01:23 +0530

    Update SmileCapture.dart

--- lib/FeelingExplore/SmileCapture.dart
+++ lib/FeelingExplore/SmileCapture.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 import '../color.dart';
 
@@ -14,13 +15,24 @@ class SmileCapture extends StatelessWidget {
           color: primaryColor,
         ),
         title: Text(
-          'Capture Smile',
+          'Smile to Camera',
           style: GoogleFonts.sourceSans3(
             fontSize: 24,
             fontWeight: FontWeight.w700,
             color: primaryColor,
           ),
         ),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(child: Lottie.asset('assets/animations/smile.json',
+                  width: 160, height: 160),),
+              Expanded(child: Text('When you Smile.you are so Beautiful'))
+            ],
+          )
+        ],
       ),
     );
   }
